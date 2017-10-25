3de8bea57c6f9bfb140612e19c7f1671133a9fb5
taylorwrt <taylorwrt@192.168.82.174>
2017-10-25 15:56:38 -0400

    Applied NetBeans source formatter to change style of braces.

--- src/main/java/com/nrims/common/RandomAccessEndianFile.java
+++ src/main/java/com/nrims/common/RandomAccessEndianFile.java
@@ -2,8 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 package com.nrims.common;
 
 import java.io.*;
@@ -12,162 +10,154 @@ import java.io.*;
  *
  * @author bepstein
  */
-public class RandomAccessEndianFile extends RandomAccessFile
-{
+public class RandomAccessEndianFile extends RandomAccessFile {
+
     /* Constructors */
     /**
      * Initialises the object
+     *
      * @param file
      * @param mode
      * @throws FileNotFoundException
      */
     public RandomAccessEndianFile(File file, String mode)
-            throws FileNotFoundException
-    {
+            throws FileNotFoundException {
         super(file, mode);
         init_local_variables();
     }
 
     /**
      * Initializes the object.
+     *
      * @param name
      * @param mode
      * @throws FileNotFoundException
      */
     public RandomAccessEndianFile(String name, String mode)
-            throws FileNotFoundException
-    {
+            throws FileNotFoundException {
         super(name, mode);
         init_local_variables();
     }
 
     /* public variables */
 
-    /* public methods */
-
+ /* public methods */
     /**
      * Sets the internal boolean flag for big endian to true for big endian,
      * false for little endian.
+     *
      * @param flag
      */
-    public void setBigEndianFlag(boolean flag)
-    {
+    public void setBigEndianFlag(boolean flag) {
         big_endian_flag = flag;
     }
 
     /**
      * Returns big endian flag
+     *
      * @return boolean big endian flag
      */
-    public boolean getBigEndianFlag()
-    {
-        return( big_endian_flag );
+    public boolean getBigEndianFlag() {
+        return (big_endian_flag);
     }
-
 
     /**
      * Read a double value correcting for the proper endian structure.
+     *
      * @return the read-in double
      * @throws IOException
      */
     public double readDoubleEndian()
-            throws IOException
-    {
+            throws IOException {
         double ret_value;
         ret_value = super.readDouble();
 
-        if ( ! getBigEndianFlag() )
-        {
+        if (!getBigEndianFlag()) {
             ret_value = DataUtilities.doubleReverseByteOrder(ret_value);
         }
 
-        return( ret_value );
+        return (ret_value);
     }
 
     /**
      * Read a float correcting for the proper endian structure.
+     *
      * @return read-in float
      * @throws IOException
      */
     public float readFloatEndian()
-            throws IOException
-    {
+            throws IOException {
         float ret_value;
         ret_value = super.readFloat();
 
-        if ( ! getBigEndianFlag() )
-        {
+        if (!getBigEndianFlag()) {
             ret_value = DataUtilities.floatReverseByteOrder(ret_value);
         }
 
-        return( ret_value );
+        return (ret_value);
     }
 
     /**
      * Read s short correcting for the proper endian structure.
+     *
      * @return read-in short
      * @throws IOException
      */
     public short readShortEndian()
-            throws IOException
-    {
+            throws IOException {
         short ret_value;
         ret_value = super.readShort();
-        
-        if ( ! getBigEndianFlag() )
-        {
+
+        if (!getBigEndianFlag()) {
             ret_value = DataUtilities.shortReverseByteOrder(ret_value);
         }
 
-        return( ret_value );
+        return (ret_value);
     }
 
     /**
      * Read an int correcting for the proper endian structure.
+     *
      * @return read-in int
      * @throws IOException
      */
     public int readIntEndian()
-            throws IOException
-    {
+            throws IOException {
         int ret_value;
         ret_value = super.readInt();
 
-        if ( ! getBigEndianFlag() )
-        {
+        if (!getBigEndianFlag()) {
             ret_value = DataUtilities.intReverseByteOrder(ret_value);
         }
 
-        return( ret_value );
+        return (ret_value);
     }
 
     /**
      * Read a long correcting for the proper endian structure.
+     *
      * @return read-in long
      * @throws IOException
      */
     public long readLongEndian()
-            throws IOException
-    {
+            throws IOException {
         long ret_value;
         ret_value = super.readLong();
 
-        if ( ! getBigEndianFlag() )
-        {
+        if (!getBigEndianFlag()) {
             ret_value = DataUtilities.longReverseByteOrder(ret_value);
         }
 
-        return( ret_value );
+        return (ret_value);
     }
 
     /* private constants */
 
-    /* private variables */
+ /* private variables */
     private boolean big_endian_flag;
 
     /* private methods */
-    private void init_local_variables()
-    {
-        setBigEndianFlag( true );
+    private void init_local_variables() {
+        setBigEndianFlag(true);
     }
 }
